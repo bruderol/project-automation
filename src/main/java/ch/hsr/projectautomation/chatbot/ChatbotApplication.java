648c4ff55ac97d98dd4fa38b364c1d5e6dda0f07
Rolf Bruderer <bruderer@computerscience.ch>
2019-04-26 20:35:24 +0200

    Hello World Spring Boot App with Gradle Build

---
+++ src/main/java/ch/hsr/projectautomation/chatbot/ChatbotApplication.java
@@ -0,0 +1,13 @@
+package ch.hsr.projectautomation.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotApplication.class, args);
+	}
+
+}
